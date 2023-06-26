a9d9ba6c31a7916506e7700e23e46be644e42671
tentalha <megakiller190@gmail.com>
2023-06-27 02:53:43 +0500

    test commit

--- server/routes/root.routes.js
+++ server/routes/root.routes.js
@@ -1,8 +1,7 @@
+//In this file all the routes are merged and then exported as single route object.
 import { Router } from "express";
 import testRoute from "./test.routes";
-
 // --------------------------------------------------------->>
-
 export const router = Router();
 
 router.use("/", testRoute);
